67b27342cb6385866aa7728a3b3755ec8de0d269
THIRUSAKTHI1405 <159860628+THIRUSAKTHI1405@users.noreply.github.com>
2024-02-24 11:21:11 +0530

    Update mirror arrays.c

--- 23-02-2024/mirror arrays.c
+++ 23-02-2024/mirror arrays.c
@@ -1,10 +1,17 @@
 /*
-The program must accept three integers X, Y and K as the input. The program must print the count of integers from X to Y which contains the integer K as a subset. Boundary Condition(s): 1 <= X< Y <= 10^8 1 <= K <= Y Input Format: The first line contains X, Y and K separated by a space. Output Format: The first line contains the count of integers from X to Y which contains the integer K as a subset. Example Input/Output 1:
- Input: 100 300 25
- Output 12
- Explanation: There are 12 integers from 100 to 300 which contains the integer 25 as a subset 125 25 250 251 252 253 254 255 256 257 258 259 Example Input/Output 2:
- Input: 4987 8959 510
- Output 14
+input
+4
+10 20 30 40
+04 30 20 10
+output
+1
+input
+7
+12 13 14 15 16 17
+17 16 15 18 13 19
+output 0
+
+the reversing two array must be in the same format
 */
 
 #include<stdio.h>  
